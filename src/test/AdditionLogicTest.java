57a3a9432755a8e589e7664a30d7cd7b6093d2ea
sushil.soni <sushil.soni@accenture.com>
2015-04-22 15:10:07 +0530

    first commmit

---
+++ src/test/AdditionLogicTest.java
@@ -0,0 +1,18 @@
+
+import test.devops.servlet.AdditionLogic;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdditionLogicTest {
+
+	@Test
+	public final void testAddition() {
+		assertEquals("10+30 must be 40",40, AdditionLogic.addition(10,30));
+		assertEquals("30+30 must be 60",60, AdditionLogic.addition(30,30));
+		assertEquals("0+30 must be 30",30, AdditionLogic.addition(0,30));	
+	}
+
+}
+
+
